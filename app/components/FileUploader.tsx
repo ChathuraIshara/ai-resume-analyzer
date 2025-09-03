aaeead280c11590c40cfe432216613a258555d50
Chathura Ishara <112225216+ChathuraIshara@users.noreply.github.com>
2025-09-03 23:16:01 +0530

    Add singer details

---
+++ app/components/FileUploader.tsx
@@ -0,0 +1,82 @@
+import React, { useCallback } from "react";
+import { useDropzone } from "react-dropzone";
+import { useState } from "react";
+
+interface FileUpladerProps {
+  onFileSelect?: (file: File | null) => void;
+}
+
+function formatSize(bytes: number): string {
+  if (bytes < 1024) return `${bytes} B`;
+  if (bytes < 1024 * 1024) return `${(bytes / 1024).toFixed(2)} KB`;
+  if (bytes < 1024 * 1024 * 1024)
+    return `${(bytes / (1024 * 1024)).toFixed(2)} MB`;
+  return `${(bytes / (1024 * 1024 * 1024)).toFixed(2)} GB`;
+}
+
+const FileUploader = ({ onFileSelect }: FileUpladerProps) => {
+  const onDrop = useCallback(
+    (acceptedFiles: File[]) => {
+      const file = acceptedFiles[0] || null;
+      onFileSelect?.(file);
+    },
+    [onFileSelect]
+  );
+  const { getRootProps, getInputProps, isDragActive, acceptedFiles } =
+    useDropzone({
+      onDrop,
+      multiple: false,
+      accept: { "application/pdf": [".pdf"] },
+      maxSize: 20 * 1024 * 1024,
+    });
+  const file = acceptedFiles[0] || null;
+  const maxFileSize = 20 * 1024 * 1024; // 20 MB in bytes
+
+  return (
+    <div className="w-full gradient-border">
+      <div {...getRootProps()}>
+        <input {...getInputProps()} />
+        <div className="space-y-4 cursor-pointer">
+          {file ? (
+            <div
+              className="uploader-selected-file"
+              onClick={(e) => e.stopPropagation()}
+            >
+                 <img src="/images/pdf.png" alt="pdf" className="size-10" />
+              <div className="flex items-center space-x-3">
+                <div>
+                  <p className="text-lg text-gray-700 font-medium truncate max-w-xs">
+                    {file.name}
+                  </p>
+                  <p className="text-sm text-gray-500">
+                    {formatSize(file.size)}
+                  </p>
+                </div>
+              </div>
+              <button className="p-2 cursor-pointer" onClick={(e)=>{
+                onFileSelect?.(null)
+                }}>
+                <img src="/icons/cross.svg" alt='remove' className="w-4 h-4"/>
+              </button>
+            </div>
+          ) : (
+            <div>
+              <div className="mx-auto w-16 h-16 flex items-center justify-center mb-2">
+                <img src="/icons/info.svg" alt="upload" className="size-20" />
+              </div>
+              <p className="text-lg text-gray-500">
+                <span className="font-semibold">Click to upload</span> or drag
+                and drop
+              </p>
+              <p className="text-lg text-gray-500">
+                PDF (max {formatSize(maxFileSize)})
+              </p>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FileUploader;
---
+++ app/routes/upload.tsx
@@ -0,0 +1,63 @@
+import React, { type FormEvent } from 'react'
+import Navbar from '~/components/Navbar'
+import { useState } from 'react';
+import FileUploader from '~/components/FileUploader';
+
+const upload = () => {
+   const [isProcessing,setIsProcessing] = useState(false);
+   const [statusText,setStatusText] = useState('');
+   const [file,setFile] = useState<File | null>(null);
+ 
+   const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+      event.preventDefault();
+   }
+
+   const handleFileSelect = (file:File | null) =>{
+    setFile(file);
+   }
+
+  return (
+   <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+      <Navbar />
+      <section className="main-section">
+        <div className='page-heading'>
+            <h1>Smart feedback for your dream job</h1>
+            {isProcessing ?(
+                <>
+                <p>{statusText}</p>
+                <img src='/images/resume-scan.gif' className='w-full'/>
+                </>
+            ):(
+               <>
+               <h2>Drop your resume for an ATS score and improvement tips</h2>
+               </>
+            )}
+            {!isProcessing && (
+                <form id='upload-form' onSubmit={handleSubmit} className='flex flex-col gap-4 mt-8'> 
+                 <div className='form-div'>
+                    <label htmlFor='company-name'>Company Name</label>
+                    <input type='text' id='company-name' name='company-name' placeholder='Company Name' required />
+                 </div>
+                <div className='form-div'>
+                    <label htmlFor='job-title'>Job Title</label>
+                    <input type='text' id='job-title' name='job-title' placeholder='Job Title' required />
+                 </div>
+                  <div className='form-div'>
+                    <label htmlFor='job-description'>Job Description</label>
+                    <textarea rows={5}  id='job-description' name='job-description' placeholder='Job Description' required />
+                 </div>
+                   <div className='form-div'>
+                    <label htmlFor='job-description'>Upload Resume</label>
+                    <FileUploader onFileSelect={handleFileSelect}/>
+                 </div>
+                 <button className='primary-button' type='submit'>Analyze Resume</button>
+
+                </form>
+            )}
+        </div>
+        </section>
+    </main>     
+  )
+}
+
+export default upload
